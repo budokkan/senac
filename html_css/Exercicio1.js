653aa4d180f725f939ce69879f50920dca951424
budokkan <jp599769@gmail.com>
2024-10-25 11:53:14 -0300

    html

---
+++ html_css/Exercicio1.js
@@ -0,0 +1,48 @@
+//1. Escreva um algoritmo que receba dois números e exiba a soma. 
+var a = ("inteira")
+var b = ("inteira")
+var soma = ("inteira")
+
+a = 10
+b = 20
+soma = a + b 
+
+exibir (soma)
+
+//2. Escreva um algoritmo que receba três números e exiba a soma deles. 
+var a = ("inteira")
+var b = ("inteira")
+var c = ("inteira")
+var soma = ("inteira")
+
+a = 10
+b = 20
+c = 30
+soma = a + b + c 
+
+exibir (soma) 
+
+//3.Escreva um pseudocódigo para calcular a média de três notas. 
+var nota1 = ("real")
+var nota2 = ("real")
+var nota3 = ("real")
+
+nota1 = 1,5
+nota2 = 2,5
+nota3 = 3,5
+media = (nota1 + nota2 + nota3) /3 
+
+exiber (media)
+
+//4.Crie um algoritmo que leia um número e exiba o dobro desse número.
+var numero = ("inteira")
+var doblo = ("inteiro")
+var soma = ("inteira")
+
+numero = 4
+doblo = 2 * numero
+
+//5.Desenvolva um pseudocódigo que leia a base e a altura de um retângulo e calcule sua área. 
+
+var base 
+var altura 
